18a7d0ef3022fef448bfc6653196d615570d1f10
Shyam Vishwakarma <144812100+Shyam-Vishwakarma@users.noreply.github.com>
2024-10-14 19:41:11 +0530

    initial commit

--- src/main/java/com/infosys/carRentalSystem/controller/LoginController.java
+++ src/main/java/com/infosys/carRentalSystem/controller/LoginController.java
@@ -9,6 +9,6 @@ public class LoginController {
 
     @GetMapping("/index")
     public ModelAndView showIndexPage() {
-        return new ModelAndView("index"); // This will resolve to /WEB-INF/jsp/index.jsp
+        return new ModelAndView("index");
     }
 }
